5b702b3809461c3bb509f0da7d424df7f50ad4e8
Danial <d.senejohnny@gmail.com>
2020-09-25 11:13:59 +0200

    first git and dvc initialization

---
+++ code.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+data = {'Name':['Tom', 'Jack', 'nick', 'juli'], 'Age':[40, 11, 78, 28]}
+df = pd.DataFrame(data=data)
+# df['Hobby'] = ['Fotball', 'Video Games', 'Reading', 'Running']
+df.to_csv('data/df.csv')
+
